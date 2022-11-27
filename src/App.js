a0a540803f13a9beb736e85f16227c30097957d7
spyros zaboukis <spyros.zaboukis.83@gmail.com>
2022-11-27 21:17:10 +0100

    send nav bar and logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About'
+import './styles/index.scss'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element = { <Home/> }/>
+        <Route path='/About' element={ <About/>} />
+        <Route path='*' element = { <Home/> }/>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/About.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo/>
+            <Navigation/>
+            <h1>About us</h1>
+            
+                <p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                "Il ny a personne qui naime la souffrance pour elle-même, qui ne la recherche et qui ne la veuille pour elle-même..."</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In blandit nulla in sapien elementum, 
+                vel eleifend tellus ornare. Nunc et velit id mi dapibus eleifend. Aliquam ac tempus neque. 
+                Integer placerat efficitur ex non consectetur. Nunc ex eros, elementum vitae quam et, semper 
+                volutpat eros. Praesent et tempor est, vitae dignissim leo. Praesent massa nunc, pretium nec mi at, 
+                lobortis ornare mi. Duis finibus lorem quis fringilla porttitor. Donec elementum pulvinar egestas. 
+                Pellentesque est diam, fermentum vitae turpis quis, scelerisque suscipit nibh. Donec ut pretium odio. 
+                Sed ultrices molestie elit, non laoreet dui. Phasellus mollis nec orci vel faucibus.</p>
+
+                <p>Donec nec ultricies erat, non aliquam lorem. Nam sed dolor metus. Nam aliquam, justo sit amet tincidunt 
+                porttitor, quam risus lobortis arcu, viverra tristique magna tellus et enim. Suspendisse vel ex nec magna 
+                porttitor pellentesque eu et lacus. Cras et nibh scelerisque, aliquet enim et, ultricies erat. Donec interdum 
+                orci a nulla mattis, in iaculis ante rutrum. Pellentesque aliquet eleifend dolor ultricies aliquam. Curabitur 
+                sagittis elit ut ullamcorper vulputate. Morbi sodales lorem mollis lacus congue, sed vestibulum ligula efficitur. 
+                Cras vel consequat elit. Pellentesque vestibulum elit sit amet dui pulvinar lacinia. Ut vel elementum justo. 
+                Proin tincidunt placerat ipsum a fringilla.</p>
+
+                <p>Maecenas metus erat, sagittis sed ante eu, vehicula aliquet neque. Aenean porttitor porttitor erat, 
+                non mattis sem facilisis sit amet. Suspendisse et nisi eget lorem ultricies semper at id nunc. 
+                Proin ullamcorper ut urna nec accumsan. Quisque nec gravida enim, non accumsan mauris. Mauris eu augue ullamcorper, 
+                laoreet risus vitae, efficitur orci. Etiam mi purus, dictum nec dolor ac, maximus rhoncus nibh. 
+                Fusce auctor suscipit risus id lacinia. Aliquam quam dolor, tristique sed dignissim et, tempus eu ex. Phasellus 
+                tellus metus, dignissim ac dui a, ultrices iaculis mauris. Quisque egestas fermentum enim, ut scelerisque purus hendrerit a. 
+                Quisque vel sem mauris. Donec tempus, mauris sit amet malesuada egestas, enim nulla convallis urna, ac fermentum lacus tellus eu nibh. 
+                Duis non metus ac ex commodo lacinia. Phasellus maximus ex magna, vel pretium quam blandit porta.</p>
+
+                <p>Cras pharetra, ipsum sit amet viverra pharetra, lorem nulla placerat justo, sit amet tincidunt libero arcu et velit. 
+                Aenean mollis lectus ut ex consequat, vel pulvinar ipsum euismod. Sed vitae maximus urna, non convallis sem. Morbi ultricies, 
+                est et imperdiet auctor, arcu orci dignissim nulla, quis mollis mauris nibh accumsan magna. Praesent viverra ex nec mattis tempor. 
+                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc sit amet quam ac tortor faucibus
+                pellentesque eu quis quam. Praesent at vehicula tellus. Pellentesque nec enim tristique, volutpat eros sit amet, tincidunt enim. 
+                Pellentesque tortor nulla, malesuada ac lobortis eu, eleifend sit amet mi. Nunc fringilla porttitor quam tincidunt semper. Vivamus 
+                ligula mauris, dictum et maximus sed, volutpat at dolor. Morbi id sapien sit amet velit ultrices posuere sed ac lorem. 
+                Maecenas sit amet tempus magna, vel aliquam ex.</p>
+
+                <p>Ut neque massa, fringilla tincidunt luctus eget, facilisis eget libero. Praesent et ex vel erat pulvinar consequat ac a nunc. 
+                Suspendisse mollis sit amet orci non ornare. Quisque tempor eleifend maximus. Vivamus eu tristique sapien, quis fermentum orci. 
+                Maecenas sit amet dolor scelerisque, efficitur neque sagittis, auctor sem. Ut pharetra purus a nunc tempor, 
+                blandit elementum arcu facilisis. Sed mauris dolor, bibendum vel condimentum vitae, tincidunt ut felis.</p>
+
+                <p>Nunc faucibus augue non turpis porttitor consequat. Donec et malesuada libero. Maecenas vitae ante mauris.</p> 
+                
+                <p>Nulla facilisi. Quisque eget urna vitae ante sollicitudin fermentum.</p>
+        </div>
+    );
+};
+
+export default About;
